1b4c9620d825e39e0b2f25eb956f9a365b051575
Rodion Bolkunevych <rbolck@gmail.com>
2016-10-04 10:45:37 +0300

    Update BaseController

--- vendor/framework/Controller/BaseController.php
+++ vendor/framework/Controller/BaseController.php
@@ -4,10 +4,21 @@ namespace Framework\Controller;
 
 use Framework\Response\Response;
 
+/**
+ * Class BaseController
+ * Implement parent class for controllers
+ * @package Framework\Controller
+ */
 class BaseController
 {
-    public function render($view, $params)
+    /**
+     * @param $view
+     * @param $params
+     * @param $code
+     * @return Response
+     */
+    public function render($view, $params, $code)
     {
-            return new Response();
+        return new Response($params, $code);
     }
 }
