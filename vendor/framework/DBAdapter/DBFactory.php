770d2455ea4c5db82b5ac7cc6710d3f14da45edf
Rodion Bolkunevych <rbolck@gmail.com>
2016-10-04 10:46:38 +0300

    Add PHPDocs

--- vendor/framework/DBAdapter/DBFactory.php
+++ vendor/framework/DBAdapter/DBFactory.php
@@ -3,9 +3,20 @@
 namespace Framework\DBAdapter;
 
 
+/**
+ * Class DBFactory
+ * Implement Factory for get connection to DB
+ * @package Framework\DBAdapter
+ */
 class DBFactory
 {
 
+    /**
+     * @param $driver
+     * @param $config
+     * @return mixed
+     * @throws \Exception
+     */
     public static function getConnection($driver, $config)
     {
         $driver = 'Framework\\DBAdapter\\' . $driver;
